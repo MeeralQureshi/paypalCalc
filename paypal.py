8f6c766371fa12428966798dd5fac7732681a377
Udara Weerasinghege <udara.weerasinghege@mail.utoronto.ca>
2015-07-18 16:04:11 -0400

    setup npm

---
+++ paypal.py
@@ -0,0 +1,6 @@
+import paypalrestsdk
+paypalrestsdk.configure({
+  'mode': 'sandbox',
+  'client_id': 'AQkquBDf1zctJOWGKWUEtKXm6qVhueUEMvXO_-MCI4DQQ4-LWvkDLIN2fGsd',
+  'client_secret': 'EL1tVxAjhT7cJimnz5-Nsx9k2reTKSVfErNQF-CmrwJgxRtylkGTKlU4RvrX'
+})
