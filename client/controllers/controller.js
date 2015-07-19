8ca9864ea786218909cac214f72cfa51c4e2b085
Meeral Qureshi <meeralqureshi@hotmail.com>
2015-07-19 00:50:39 -0400

    Validated fields

--- client/controllers/controller.js
+++ client/controllers/controller.js
@@ -2,4 +2,7 @@ var payPal = angular.module('payPal', []);
 
 payPal.controller('payCtrl', ['$scope', '$http', function($scope, $http){
   console.log('sup from controlelr');
+  $scope.Submit = function Submit(){
+  	console.log("submit");
+  }
 }])
