bade83c88fd1a128036670c9e984655f2e04968a
Udara Weerasinghege <udara.weerasinghege@mail.utoronto.ca>
2015-07-19 03:35:35 -0400

    asdf

--- client/controllers/controller.js
+++ client/controllers/controller.js
@@ -1,5 +1,15 @@
 var payPal = angular.module('payPal', []);
 
 payPal.controller('payCtrl', ['$scope', '$http', function($scope, $http){
-  console.log('sup from controlelr');
+  // Simple POST request example (passing data) :
+$http.post('/postPayPal', {msg:'hello word!'}).
+  success(function(data, status, headers, config) {
+    // this callback will be called asynchronously
+    // when the response is available
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
+
 }])
--- server/server.js
+++ server/server.js
@@ -9,5 +9,9 @@ var pathName = '/Users/udaraweerasinghege/personal/BattleHacks/paypalCalc/client
 
 app.use(express.static(pathName));
 
+app.get('/postPayPal', function(req, res) {
+  console.log('i got it');
+});
+
 app.listen(3000);
 console.log("Express server listening on port 3000");
